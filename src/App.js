2ca1d2f865225529d0acae87b5960af240d50092
ng0035 <nikitagupta0809@gmail.com>
2019-01-24 13:18:02 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+
+        <input type = "checkbox"/>
+        <p>Cricket</p>
+        <input type = "checkbox"/>
+        <p>Football</p>        
+        <input type = "checkbox"/>
+        <p>Wrestling</p>
+        <input type = "checkbox"/>
+        <p>Badminton</p>
+      </div>
+    );
+  }
+}
+
+export default App;
